34065e551045db6d87439df6f763d001b2ef4af0
Jan-Philipp Benecke <jpb@cleverreach.com>
2020-10-14 20:50:06 +0200

    File parameter

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/jhillyerd/enmime v0.8.2
 	github.com/olekukonko/tablewriter v0.0.4
+	github.com/sirupsen/logrus v1.7.0
 )
--- main.go
+++ main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"github.com/jhillyerd/enmime"
 	"github.com/jpbede/eop-header/eop"
+	log "github.com/sirupsen/logrus"
 	"io/ioutil"
+	"os"
 )
 
+var filePath string
+
+func init() {
+	flag.StringVar(&filePath, "file", "", "Path to EML file")
+}
+
 func main() {
-	rawMail, _ := ioutil.ReadFile("./unnamed_attachment_1 (13).eml")
+	flag.Parse()
+	if filePath == "" {
+		log.Fatal("Missing file path")
+		os.Exit(1)
+	}
+
+	rawMail, _ := ioutil.ReadFile(filePath)
 
 	br := bytes.NewReader(rawMail)
 	env, _ := enmime.ReadEnvelope(br)
