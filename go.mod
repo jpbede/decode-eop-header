e7d9f71465fb998087058ba94e2505a894735e8f
Jan-Philipp Benecke <jpb@cleverreach.com>
2020-10-29 09:15:18 +0100

    Using now urfave cli

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/jpbede/eop-header
 go 1.15
 
 require (
-	github.com/davecgh/go-spew v1.1.1
 	github.com/jhillyerd/enmime v0.8.2
 	github.com/olekukonko/tablewriter v0.0.4
 	github.com/sirupsen/logrus v1.7.0
+	github.com/urfave/cli/v2 v2.2.0
 )
--- main.go
+++ main.go
@@ -2,34 +2,58 @@ package main
 
 import (
 	"bytes"
-	"flag"
+	"fmt"
 	"github.com/jhillyerd/enmime"
 	"github.com/jpbede/eop-header/eop"
 	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
 	"io/ioutil"
 	"os"
 )
 
-var filePath string
-var tableWidth int
-
-func init() {
-	flag.StringVar(&filePath, "file", "", "Path to EML file")
-	flag.IntVar(&tableWidth, "tableWidth", 80, "Width of the result table")
-}
+var (
+	version = "dev"
+)
 
 func main() {
-	flag.Parse()
-	if filePath == "" {
-		log.Fatal("Missing file path")
-		os.Exit(1)
+	app := &cli.App{
+		Name:    "eop-header",
+		Usage:   "Decodes Microsoft's Exchange Online Protection header into a nice view",
+		Version: version,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "file",
+				Aliases:  []string{"f"},
+				Required: true,
+				Usage:    "Path to EML file",
+			},
+			&cli.IntFlag{
+				Name:    "tableWidth",
+				Aliases: []string{"t"},
+				Value:   80,
+				Usage:   "Width of the result table",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			if c.Bool("version") {
+				fmt.Print(version)
+				return nil
+			}
+
+			rawMail, _ := ioutil.ReadFile(c.String("file"))
+
+			br := bytes.NewReader(rawMail)
+			env, _ := enmime.ReadEnvelope(br)
+
+			parser := eop.NewParserWithEnvelop(env)
+			parser.ParseAndRender(c.Int("tableWidth"))
+
+			return nil
+		},
 	}
 
-	rawMail, _ := ioutil.ReadFile(filePath)
-
-	br := bytes.NewReader(rawMail)
-	env, _ := enmime.ReadEnvelope(br)
-
-	parser := eop.NewParserWithEnvelop(env)
-	parser.ParseAndRender(tableWidth)
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
