0d693885ffaafc3b0a68caa7b371f223c803f1af
Jan-Philipp Benecke <jpb@cleverreach.com>
2020-10-29 22:08:56 +0100

    Auth-Result parsing

--- eop/auth-result.go
+++ eop/auth-result.go
@@ -1,5 +1,34 @@
 package eop
 
+import (
+  "strings"
+)
+
 func (parser *Parser) ParseAuthenticationResult() {
-	//header := parser.envelope.GetHeader("Authentication-Results")
+  header := parser.envelope.GetHeader("Authentication-Results")
+  parts := strings.Split(header, "=")
+  fields := make(map[string]string)
+
+  stopwords := []string{"spf", "dkim", "dmarc", "action", "smtp.mailfrom",
+    "header.d", "header.from", "compauth", "action", "reason"}
+
+  // TODO: I don't like this
+
+  // match parts
+  for ip, part := range parts{
+    for _, stopword := range stopwords {
+      if strings.Contains(part, stopword) {
+        fields[stopword] = parts[ip+1]
+      }
+    }
+  }
+
+  // now remove stop words from parts
+  for key, value := range fields {
+    for _, stopword := range stopwords {
+      if strings.Contains(value, stopword) {
+        fields[key] = strings.TrimSpace(strings.Split(value, stopword)[0])
+      }
+    }
+  }
 }
